996bc80ab4ac9c26e88ff7a21742497073627838
shimon <shimonbhuiya02@gmail.com>
2025-11-09 23:42:19 +0600

    init Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:coffe_shop/screens/welcome_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFF212325)
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+
+}
