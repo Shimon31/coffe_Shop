79f942878f0f968bbe628f2c4570b5848ebc6862
shimon <shimonbhuiya02@gmail.com>
2026-03-02 02:26:43 +0600

    welcome Screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,3 @@
-import 'package:coffe_shop/screens/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,63 +9,34 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Container(
-        padding: EdgeInsets.only(top: 100, bottom: 40),
+        padding: EdgeInsets.only(top: 100,bottom: 40),
         decoration: BoxDecoration(
-          color: Colors.white,
-          image: DecorationImage(
-            image: AssetImage("images/bg.png"),
-            fit: BoxFit.cover,
-            opacity: 0.9,
+          color: Colors.black,
+          image: DecorationImage(image: AssetImage("images/bg.png"),
+          fit: BoxFit.cover,
+              opacity: .9
           ),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
-              "Coffee Shop",
-              style: GoogleFonts.pacifico(fontSize: 50, color: Colors.white),
+            Text("Coffee Shop",
+              style: GoogleFonts.pacifico(
+                fontSize: 50,
+                color: Colors.white
+              )
             ),
-            Column(
-              children: [
-                Text(
-                  "Feeling Low? Take a Sip of Coffee",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(height: 80),
-                Material(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.orangeAccent,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=> HomeScreen()));
-                    },
-                      child: Container(
-                        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 35),
-                        decoration: BoxDecoration(
-                          color: Colors.orangeAccent,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Text(
-                          "Get Start",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1,
-                          ),
-                        ),
-                      ),
-                    ),
-                ),
-              ],
-            ),
+            Column(children: [
+              Text("Feeling Low? Take a Sip of Coffee",style: TextStyle(
+                color: Colors.white,
+                fontSize: 18
+              ),),
+
+            ],)
           ],
         ),
       ),
     );
   }
 }
+
