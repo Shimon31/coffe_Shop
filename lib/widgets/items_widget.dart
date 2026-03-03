83f5b56815d667d5b9a46cc9476ff15877b99312
shimon <shimonbhuiya02@gmail.com>
2026-03-04 00:34:50 +0600

    add Items

--- lib/widgets/items_widget.dart
+++ lib/widgets/items_widget.dart
@@ -2,10 +2,67 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ItemsWidget extends StatelessWidget {
-  const ItemsWidget({super.key});
+  List img = [
+    'latte',
+    'espresso',
+    'cold_coffee',
+    'black_coffee',
+
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GridView.count(
+      physics: NeverScrollableScrollPhysics(),
+      crossAxisCount: 2,
+      shrinkWrap: true,
+      childAspectRatio: (150 / 195),
+      children: [
+        for (int i = 0; i < img.length; i++)
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 13),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Color(0xFF212325),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.4),
+                  spreadRadius: 1,
+                  blurRadius: 8
+                )
+              ]
+            ),
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: (){},
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    child: Image.asset("images/${img[i]}.png",
+                    width: 120,
+                      height: 120,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 8),
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    img[i],
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                )
+
+              ],
+            ),
+          ),
+      ],
+    );
   }
 }
